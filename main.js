526c3045fc1accfa74e8bb1507ef3b553c66161f
Francesco Viccica <viccicafrancesco@gmail.com>
2025-03-01 12:04:52 +0100

    Esercitazione svolta

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ let anno = 2025;
 
 let anno_nascita = 1995;
 
+let età = anno - anno_nascita; 
+
+console.log(età);
+
+let anni_mancanti = 100 - età;
+
+console.log(anni_mancanti);
+
+console.log( `hai ${età} anni e ti mancano ${anni_mancanti} anni per compierne 100` );
